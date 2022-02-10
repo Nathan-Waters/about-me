98450f3450250077b2b82a148234f965ef33c7b5
Nathan Waters <nathanwaters94@gmail.com>
2022-02-09 21:01:12 -0800

    the final touches on 7th question

--- js/app.js
+++ js/app.js
@@ -1,78 +1,86 @@
 'use strict';
 
 // // console.log('hey world hey!');
-
+let totalScore = 0;
 let userName = prompt('Hello, human. What is your name?');
 
-// alert('It is great to meet you ' + userName + '. Now please take a minuite to learn about my second favorite human, Nathan.');
+alert('It is great to meet you ' + userName + '. Now please take a minuite to learn about my second favorite human, Nathan.');
 
-// let answerOne = prompt('Does Nathan have kids? beep boop.').toLowerCase();
+let answerOne = prompt('Does Nathan have kids? beep boop.').toLowerCase();
 
-// if(answerOne === 'yes' || answerOne ==='y'){
-//   // console.log('incorrect question 1');
-//   alert('No he does not, but he has 3 very cute neices!');
-// } else if (answerOne === 'no' || answerOne ==='n'){
-//   // console.log('correct question 1');
-//   alert('That would be correct! Great job human!');
-// }
+if(answerOne === 'yes' || answerOne ==='y'){
+  // console.log('incorrect question 1');
+  alert('No he does not, but he has 3 very cute neices!');
+} else if (answerOne === 'no' || answerOne ==='n'){
+  // console.log('correct question 1');
+  alert('That would be correct! Great job human!');
+  totalScore++;
+}
 
-// let answerTwo = prompt('Does Nathan like golf? beep boop.').toLowerCase();
+let answerTwo = prompt('Does Nathan like golf? beep boop.').toLowerCase();
 
-// if(answerTwo === 'yes' || answerTwo ==='y'){
-//   // console.log('correct question 2');
-//   alert('He does! however his short game does not like him.');
-// } else if (answerTwo === 'no' || answerTwo ==='n'){
-//   // console.log('incorrect question 2');
-//   alert('Wrong! Human Nathan enjoys a round of golf from time to');
-// }
+if(answerTwo === 'yes' || answerTwo ==='y'){
+  // console.log('correct question 2');
+  alert('He does! however his short game does not like him.');
+  totalScore++;
+} else if (answerTwo === 'no' || answerTwo ==='n'){
+  // console.log('incorrect question 2');
+  alert('Wrong! Human Nathan enjoys a round of golf from time to');
+}
 
-// let answerThree = prompt('Is Nathan from Washington? beep beep boop.').toLowerCase();
+let answerThree = prompt('Is Nathan from Washington? beep beep boop.').toLowerCase();
 
-// if(answerThree === 'yes' || answerThree ==='y'){
-//   // console.log('correct question 3');
-//   alert('Correct Human! Nathan Was born and raised in Washington');
-// } else if (answerThree === 'no' || answerThree ==='n'){
-//   // console.log('incorrect question 3');
-//   alert('Wrong! Human Nathan did however live in Alaska for a short time growing up.');
-// }
+if(answerThree === 'yes' || answerThree ==='y'){
+  // console.log('correct question 3');
+  alert('Correct Human! Nathan Was born and raised in Washington');
+  totalScore++;
+} else if (answerThree === 'no' || answerThree ==='n'){
+  // console.log('incorrect question 3');
+  alert('Wrong! Human Nathan did however live in Alaska for a short time growing up.');
+}
 
-// let answerFour = prompt('Was Nathan a ranked figure skater? beep beep boop.').toLowerCase();
+let answerFour = prompt('Was Nathan a ranked figure skater? beep beep boop.').toLowerCase();
 
-// if(answerFour === 'yes' || answerFour ==='y'){
-//   // console.log('incorrect question 4');
-//   alert('Nagative. Nathan did however do speed skating!');
-// } else if (answerFour === 'no' || answerFour ==='n'){
-//   // console.log('correct question 4');
-//   alert('That is correct, but he would have been beautiful!');
-// }
+if(answerFour === 'yes' || answerFour ==='y'){
+  // console.log('incorrect question 4');
+  alert('Nagative. Nathan did however do speed skating!');
+} else if (answerFour === 'no' || answerFour ==='n'){
+  // console.log('correct question 4');
+  alert('That is correct, but he would have been beautiful!');
+  totalScore++;
+}
 
-// let answerFive = prompt('Does Nathan Like sports? beep beep boop.').toLowerCase();
+let answerFive = prompt('Does Nathan Like sports? beep beep boop.').toLowerCase();
 
-// if(answerFive === 'yes' || answerFive ==='y'){
-//   // console.log('correct question 5');
-//   alert('Correct! Nathan enjoys some good sports ball fun, beeeeep.');
-// } else if (answerFive === 'no' || answerFive ==='n'){
-//   // console.log('incorrect question 5');
-//   alert('Wrong! Sports is a good excuse to hang with friends.');
-// }
+if(answerFive === 'yes' || answerFive ==='y'){
+  // console.log('correct question 5');
+  alert('Correct! Nathan enjoys some good sports ball fun, beeeeep.');
+  totalScore++;
+} else if (answerFive === 'no' || answerFive ==='n'){
+  // console.log('incorrect question 5');
+  alert('Wrong! Sports is a good excuse to hang with friends.');
+}
 
-// for loop on the 6th question math.random() taken from https://www.w3schools.com/JS/js_random.asp
-// let randomNum = Math.floor((Math.random() * 50));
-// for(let i = 0; i <= 3; i++){
+// for loop on the 6th question math.random() taken from https:www.w3schools.com/JS/js_random.as
 
-//   // let randomNum = Math.floor((Math.random() * 50));
-//   let userGuess = prompt('pick a number between 0 and 50');
-//   if (userGuess === randomNum){
-//     alert('correct!');
-//     break;
-//   } else if(userGuess > randomNum && i !== 3){
-//     alert('incorrect, try something lower');
-//   } else if(userGuess < randomNum && i !== 3){
-//     alert('incorrect, try something higher');
-//   } else if(i === 3){
-//     alert('Sorry the correct answer was :' + randomNum);
-//   }
-// }
+
+let randomNum = Math.floor((Math.random() * 50));
+for(let i = 0; i <= 3; i++){
+
+  // let randomNum = Math.floor((Math.random() * 50));
+  let userGuess = prompt('pick a number between 0 and 50');
+  if (userGuess === randomNum){
+    alert('correct!');
+    totalScore++;
+    break;
+  } else if(userGuess > randomNum && i !== 3){
+    alert('incorrect, try something lower');
+  } else if(userGuess < randomNum && i !== 3){
+    alert('incorrect, try something higher');
+  } else if(i === 3){
+    alert('Sorry the correct answer was :' + randomNum);
+  }
+}
 
 //question 7, checking items in an array
 
@@ -86,11 +94,11 @@ while(count < 6){
   for (let i = 0; i < arr.length; i++){
     if(arr[i] === g){
       alert('You got it! ' + g + ' is great hu?!');
+      totalScore++;
       count = 7;
     }
   }
-
-  if(count === 6){
+  if(count === 6 ){
     alert('Sorry but the places I like are ' + arr);
   } else if(count===7){
     alert('Great job!');
@@ -100,6 +108,5 @@ while(count < 6){
     count++;
   }
 }
-
-
-alert('I would like to thank you human. I\'m sure you two will be the best of friends. Nathan and ' + userName + ' the dynamic duo!');
+//close out message
+alert('I would like to thank you human. I\'m sure you two will be the best of friends. Nathan and ' + userName + ' the dynamic duo! Wow you got ' + totalScore + ' questions right about Nathan! little creepy if you ask me.');
